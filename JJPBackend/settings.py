aacc5ba96ae993c5a7f132a0845f4968ed922c39
nalinilokeshkumar <nalinilokesh13@gmail.com>
2026-05-16 05:41:17 +0530

    Space

--- JJPBackend/settings.py
+++ JJPBackend/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
- DEBUG = False
+DEBUG = False
 
 
 # ALLOWED_HOSTS = []
- ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 
 
 
